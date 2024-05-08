a6efc3bef2e134aeed2b884ad34f2e12b92dd362
princebeyond <amoodeyond222@gmail.com>
2024-05-08 14:54:45 -0700

    successs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': '0x16-api_advanced:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for HTTP errors
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
-        
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            return 0
-
-    except requests.RequestException:  # Handle any request exceptions
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
